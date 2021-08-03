587ec5a15107c7c8b80162fa4307a409dd94530c
Mostafa Razavi <mostafa@sepent.com>
2021-08-03 08:34:16 +0200

    Add popping sound and effect

--- CircleCrusher.gd
+++ CircleCrusher.gd
@@ -9,7 +9,13 @@ var ring = null
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		if not get_parent().watering:
-			queue_free()
+			# disable all collisions, as if this object doesn't exist anymore
+			collision_layer = 0
+			collision_mask = 0
+			$pop_tween.interpolate_property(self, "scale", Vector2(1, 1), Vector2(0.01, 0.01), 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+			$pop_tween.interpolate_property($sprite, "modulate:a", 1.0, 0.2, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			$pop_tween.start()
+			$pop_sound.play()
 
 
 func _physics_process(delta):
@@ -31,3 +37,7 @@ func _on_CircleCrusher_body_exited(body):
 func _on_CircleCrusher_tree_exiting():
 	if touching_ring:
 		emit_signal("lost_touch")
+
+
+func _on_pop_tween_tween_all_completed():
+	queue_free()
