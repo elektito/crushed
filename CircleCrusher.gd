fafb85751fc238c9e80172482944e8baf183712b
Mostafa Razavi <mostafa@sepent.com>
2021-08-04 07:51:01 +0200

    Add colors to circle crushers

--- CircleCrusher.gd
+++ CircleCrusher.gd
@@ -5,6 +5,41 @@ signal lost_touch()
 
 var touching_ring := false
 var ring = null
+var possible_bg_colors = [
+	Color.aqua,
+	Color.blue,
+	Color.blueviolet,
+	Color.crimson,
+	Color.greenyellow,
+	Color.darkcyan,
+	Color.darkgreen,
+	Color.darkred,
+	Color.darkorange,
+	Color.darkkhaki,
+	Color.darkviolet,
+]
+
+var possible_border_colors = [
+	Color.aqua,
+	Color.blue,
+	Color.blueviolet,
+	Color.crimson,
+	Color.greenyellow,
+	Color.darkcyan,
+	Color.darkgreen,
+	Color.darkred,
+	Color.darkorange,
+	Color.darkkhaki,
+	Color.darkviolet,
+]
+
+func _ready():
+	randomize()
+	$bg.modulate = possible_bg_colors[randi() % len(possible_bg_colors)]
+	$border.modulate = possible_bg_colors[randi() % len(possible_border_colors)]
+	while $border.modulate == $bg.modulate:
+		$border.modulate = possible_bg_colors[randi() % len(possible_border_colors)]
+
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
@@ -13,7 +48,8 @@ func _input_event(viewport, event, shape_idx):
 			collision_layer = 0
 			collision_mask = 0
 			$pop_tween.interpolate_property(self, "scale", Vector2(1, 1), Vector2(0.01, 0.01), 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
-			$pop_tween.interpolate_property($sprite, "modulate:a", 1.0, 0.2, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			$pop_tween.interpolate_property($border, "modulate:a", 1.0, 0.2, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			$pop_tween.interpolate_property($bg, "modulate:a", 1.0, 0.2, 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 			$pop_tween.start()
 			$pop_sound.play()
 
